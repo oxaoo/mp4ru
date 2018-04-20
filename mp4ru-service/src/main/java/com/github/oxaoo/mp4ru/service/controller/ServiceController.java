666bb971a3829ae4a38e778242eb33b659d2a466
oxaoo <yol0317@gmail.com>
2018-04-20 12:37:44 +0300

    GET/POST methods for parse

--- mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/controller/ServiceController.java
+++ mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/controller/ServiceController.java
@@ -7,18 +7,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/")
+@RequestMapping(value = "/parse")
 public class ServiceController {
 
     private RussianParser parser;
 
-    @RequestMapping(value = "/parse")
-    public ResponseEntity<List<String>> confirm(@RequestBody String text) {
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<List<String>> post(@RequestBody String text) {
+        try {
+            final List<String> parsedText = this.parser.parse(text);
+            return ResponseEntity.ok(parsedText);
+        } catch (final FailedParsingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<String>> get(@RequestParam("text") String text) {
         try {
             final List<String> parsedText = this.parser.parse(text);
             return ResponseEntity.ok(parsedText);
