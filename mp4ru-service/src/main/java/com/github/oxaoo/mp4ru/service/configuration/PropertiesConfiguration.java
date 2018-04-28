1c6444a9022cfad98df33a1e576e348d31edfe6b
oxaoo <yol0317@gmail.com>
2018-04-28 21:49:15 +0300

    added configuration for different environments

---
+++ mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/configuration/PropertiesConfiguration.java
@@ -0,0 +1,17 @@
+package com.github.oxaoo.mp4ru.service.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "mp4ru")
+public class PropertiesConfiguration {
+    private String workDir;
+    private String classifierModelPath;
+    private String treeTaggerPath;
+    private String parserConfigurationPath;
+}
--- mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/configuration/ServiceConfiguration.java
+++ mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/configuration/ServiceConfiguration.java
@@ -2,24 +2,24 @@ package com.github.oxaoo.mp4ru.service.configuration;
 
 import com.github.oxaoo.mp4ru.exceptions.InitRussianParserException;
 import com.github.oxaoo.mp4ru.syntax.RussianParser;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceConfiguration {
-
-    @Value("${classifierModel.path}")
-    private String classifierModelPath;
-
-    @Value("${treeTagger.path}")
-    private String treeTaggerPath;
-
-    @Value("${parserConfig.path}")
-    private String parserConfigPath;
+    private PropertiesConfiguration properties;
 
     @Bean
     public RussianParser russianParserProvider() throws InitRussianParserException {
-        return new RussianParser(this.classifierModelPath, this.treeTaggerPath, this.parserConfigPath);
+        return new RussianParser(
+                this.properties.getClassifierModelPath(),
+                this.properties.getTreeTaggerPath(),
+                this.properties.getParserConfigurationPath());
+    }
+
+    @Autowired
+    public void setProperties(final PropertiesConfiguration properties) {
+        this.properties = properties;
     }
 }
