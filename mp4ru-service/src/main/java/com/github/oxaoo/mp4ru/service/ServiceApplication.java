778ae23c4cb9d98d68c3b6c3e133cb7a27fb71d1
oxaoo <yol0317@gmail.com>
2018-04-29 01:12:36 +0300

    fix logging

--- mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/ServiceApplication.java
+++ mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/ServiceApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.nio.file.Paths;
 
 @SpringBootApplication(scanBasePackages = "com.github.oxaoo.mp4ru")
 @EnableAutoConfiguration(exclude = {
@@ -18,10 +15,6 @@ import java.nio.file.Paths;
 })
 public class ServiceApplication {
     public static void main(String[] args) {
-        final String curDir = Paths.get(".").toAbsolutePath().normalize().toString();
-        System.out.println("Current directory: " + curDir);
         SpringApplication.run(ServiceApplication.class, args);
-        final String curDir2 = Paths.get(".").toAbsolutePath().normalize().toString();
-        System.out.println("Current directory after run: " + curDir2);
     }
 }
