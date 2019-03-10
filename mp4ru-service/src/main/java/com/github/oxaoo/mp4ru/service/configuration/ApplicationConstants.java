73d72c2c4dfab735f44e2f8dc289b11812967d53
oxaoo <yol0317@gmail.com>
2019-03-11 01:27:38 +0300

    CoNLL-X data format by default #1

---
+++ mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/configuration/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package com.github.oxaoo.mp4ru.service.configuration;
+
+
+public final class ApplicationConstants {
+    public static final String MALT_PARSER_DATA_FORMAT = "DATA-FORMAT";
+    public static final String MALT_PARSER_CONLL_X_FORMAT = "CONLLX";
+    public static final String MALT_PARSER_MALT_TAB_FORMAT = "MALTTAB";
+
+    private ApplicationConstants() {
+    }
+}
--- mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/controller/ServiceController.java
+++ mp4ru-service/src/main/java/com/github/oxaoo/mp4ru/service/controller/ServiceController.java
@@ -1,39 +1,55 @@
 package com.github.oxaoo.mp4ru.service.controller;
 
 import com.github.oxaoo.mp4ru.exceptions.FailedParsingException;
-import com.github.oxaoo.mp4ru.syntax.RussianParser;
+import com.github.oxaoo.mp4ru.service.configuration.ApplicationConstants;
+import com.github.oxaoo.mp4ru.service.service.ParserService;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/parse")
+@RequestMapping(
+        value = "/parse",
+        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+)
 public class ServiceController {
 
-    private RussianParser parser;
+    private ParserService parserService;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity post(@RequestBody String text) {
-        try {
-            final List<String> parsedText = this.parser.parse(text);
-            return ResponseEntity.ok(parsedText);
-        } catch (final FailedParsingException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionUtils.getStackTrace(e));
-        }
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity post(@RequestHeader(
+            name = ApplicationConstants.MALT_PARSER_DATA_FORMAT,
+            required = false,
+            defaultValue = ApplicationConstants.MALT_PARSER_CONLL_X_FORMAT) @NotNull final String dataFormat,
+                               @RequestBody final String text) {
+        return this.parse(text, dataFormat);
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity get(@RequestParam("text") String text) {
+    public ResponseEntity get(@RequestHeader(
+            name = ApplicationConstants.MALT_PARSER_DATA_FORMAT,
+            required = false,
+            defaultValue = ApplicationConstants.MALT_PARSER_CONLL_X_FORMAT) @NotNull final String dataFormat,
+                              @RequestParam("text") final String text) {
+        return this.parse(text, dataFormat);
+    }
+
+    private ResponseEntity parse(final String text, final String dataFormat) {
         try {
-            final List<String> parsedText = this.parser.parse(text);
+            final List<?> parsedText = this.parserService.parse(text, dataFormat);
             return ResponseEntity.ok(parsedText);
         } catch (final FailedParsingException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionUtils.getStackTrace(e));
@@ -41,7 +57,7 @@ public class ServiceController {
     }
 
     @Autowired
-    public void setParser(final RussianParser parser) {
-        this.parser = parser;
+    public void setParserService(final ParserService parserService) {
+        this.parserService = parserService;
     }
 }
